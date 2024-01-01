d4531fb3ef239f4b7cbd959c63c40f3f3ac31e7e
sandeepTech90 <sandeep1990sahoo@gmail.com>
2024-01-01 23:57:35 +0530

    Todo component added

---
+++ src/components/Todos.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import "./Todo.scss";
+
+const Todos = () => {
+  const [todos, setTodos] = useState([]);
+
+  const fetchTodos = () => {
+    fetch("http://localhost:3000/")
+      .then((res) => res.json())
+      .then((res) => setTodos(res));
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  return (
+    <div className="todos">
+      {todos.map((todo) => (
+        <div className="todo">
+          <h2>{todo.title}</h2>
+          <p>{todo.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Todos;
